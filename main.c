68f0e03417900fd7a510667adeff02f3e597cb17
Rafael Ueda <rafaueeda@gmail.com>
2017-05-03 22:31:45 -0300

    Inicio da leitura de arquivos

--- main.c
+++ main.c
@@ -1,2 +1,28 @@
 #include <stdio.h>
-#include <stdin.h>
+
+char arquivo[] = "arquivos/arquivo.txt";
+
+void manipulandoArquivo();
+
+int main(){
+	
+	
+	manipulandoArquivo();
+	
+	return 0;
+}
+
+void manipulandoArquivo(){
+	FILE *arq;
+	
+	arq = fopen(arquivo, "r");
+	
+	if(arq == NULL){
+		printf("Erro na abertura do arquivo");
+	}
+	else{
+		
+	}
+	
+	fclose(arq);
+}
