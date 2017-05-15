dc91f78c680e6ad173d07bc1a875fe95dbf22719
Rafael Ueda <rafaueeda@gmail.com>
2017-05-15 19:15:40 -0300

    Implementado a quantidade de requisições

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-char arquivo[] = "arquivos/arquivo.txt";
+char arquivo[] = "arquivos/arquivo1.txt";
+
+int fitaMemoria[4] = {-1, -1, -1, -1};
+int quantidadeTrocaFrames = 0;
+int ultimaPosicao = 0;
+char ch[10];
+int valorAtual = 0;
 
 void manipulandoArquivo();
 
-int main(){
+void main(){
 	
 	
 	manipulandoArquivo();
 	
-	return 0;
+	//return 0;
 }
 
 void manipulandoArquivo(){
@@ -17,12 +24,43 @@ void manipulandoArquivo(){
 	
 	arq = fopen(arquivo, "r");
 	
-	if(arq == NULL){
-		printf("Erro na abertura do arquivo");
-	}
-	else{
+	while(!feof(arq) )
+	{
+		fgets(ch,9,arq);
+		valorAtual = atoi(ch);
+		
+		int tamanho = sizeof(fitaMemoria) / sizeof(fitaMemoria[0]);
+		if(fitaMemoria[ultimaPosicao] == -1){
+			fitaMemoria[ultimaPosicao] = valorAtual;
+			if(ultimaPosicao == 3){
+				ultimaPosicao = 0;
+			}	
+			else{
+				ultimaPosicao++;
+			}
+				
+		}
+		else{
+			int i;
+			for(i = 0; i < sizeof(fitaMemoria); i++){
+				if(fitaMemoria[i] == valorAtual)
+					continue;
+				else{
+					fitaMemoria[ultimaPosicao] = valorAtual;
+					quantidadeTrocaFrames++;
+					if(ultimaPosicao == 3)
+						ultimaPosicao = 0;
+					else
+						ultimaPosicao++;
+					break;
+				}
+			}
+			
+		}
 		
 	}
+	
+	printf("Quantidade de trocas: %d", quantidadeTrocaFrames);
 	
 	fclose(arq);
 }
